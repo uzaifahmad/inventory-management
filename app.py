cdbe83b678a975e452290e345c0de12fc00b1d6f
Uzaif Ahmad Shaikh <suzaifahmad@gmail.com>
2022-04-13 14:49:39 +0530

    Updated

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask,request,render_template
 import pyodbc
 app = Flask(__name__)
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=ZIL1180\MSSQLDEV2019;'
-                      'Database=ASSIGNMENT;'
+                      'Database=inventory;'
                       'Trusted_Connection=yes;')
 
 cursor=conn.cursor()
-cursor.execute("select * from inventory") 
-data = cursor.fetchall() #data from database
-cursor.execute("SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'inventory'") 
-content = cursor.fetchall() #data from database
+
+cursor.execute('SELECT * FROM products')
+products=cursor.fetchall()
+
 @app.route('/')
 def index():
     return render_template("form.html")
 @app.route('/example.html/')
 def invenotory(): 
-    return render_template("example.html",value=data,value1=content)
+    return render_template("example.html",value=products)
 @app.route('/sales.html/')
 def sales():
+
     return render_template("sales.html")
+
+cursor.execute('SELECT * FROM customer')
+customers=cursor.fetchall()
+
+@app.route('/customer.html/')
+def customer():
+    return render_template("customer.html",value=customers)
     
 if __name__ == '__main__':
     app.run(debug=True) 
